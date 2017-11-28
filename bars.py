fd197b2b416c50207c3adcb706611e9ec04f649b
Serg4356 <asdfg111000@yandex.ru>
2017-11-28 14:58:00 +0500

    Change data_bars to bars variable, found a mistake in get_diff_coord function, new function does not allow usage the key(=abs?) argument to simplify the code

--- bars.py
+++ bars.py
@@ -8,30 +8,31 @@ def load_data(filepath):
         loaded_data = json.loads(opened_file.read())
     return loaded_data
 
-def get_biggest_bar(data_bars):
+def get_biggest_bar(bars):
     biggest_bar_dict = dict()
-    for bar in data_bars['features']:
+    for bar in bars['features']:
         biggest_bar_dict[bar['properties']['Attributes']['SeatsCount']] = bar['properties']['Attributes']['Name']
     return biggest_bar_dict.get(max(biggest_bar_dict)),max(biggest_bar_dict)
 
 
-def get_smallest_bar(data_bars):
+def get_smallest_bar(bars):
     smallest_bar_dict = dict()
-    for bar in data_bars['features']:
+    for bar in bars['features']:
         smallest_bar_dict[bar['properties']['Attributes']['SeatsCount']] = bar['properties']['Attributes']['Name']
     return smallest_bar_dict.get(min(smallest_bar_dict)),min(smallest_bar_dict)
 
 
-def get_closest_bar(data_bars, longitude, latitude):
+def get_closest_bar(bars, longitude, latitude):
     closest_bar_dict = dict()
-    for bar in data_bars['features']:
+    for bar in bars['features']:
         closest_bar_dict[bar['properties']['Attributes']['Name']] = bar['geometry']['coordinates']
     closest_bar_dict2 = dict()
     for closest_bar_dict_key in closest_bar_dict.keys():
         closest_bar_dict2[get_diff_coord(closest_bar_dict[closest_bar_dict_key], [longitude, latitude])] = i
     return closest_bar_dict2[min(closest_bar_dict2.keys())]
 
-def get_diff_coord(coordinates,coordinates2): return math.sqrt(((coordinates[0] - coordinates2[0])+(coordinates[1] - coordinates2[1]))**2)
+def get_diff_coord(coordinates,coordinates2):
+    return abs(coordinates[0] - coordinates2[0])+abs(coordinates[1] - coordinates2[1])
 
 if __name__ == '__main__':
     print('Что нужно найти? \n 1. Наименьший бар \n 2. Наибольший бар \n 3. Ближайший бар \n ')
